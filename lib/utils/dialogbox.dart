b0f16ab6dfa089575cf0a9e76d4aa5fe2e56060e
bro_code <kelimbth@gmail.com>
2022-10-28 03:19:39 +0300

    added a TextField

--- lib/utils/dialogbox.dart
+++ lib/utils/dialogbox.dart
@@ -9,6 +9,18 @@ class DialogBox extends StatelessWidget {
       backgroundColor: Colors.blue[100],
       content: Container(
         height: 150,
+        child: Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                hintText: 'Add a new task',
+              ),
+            ), // get user input
+
+
+          ],
+        ),
+
       ),
     );
   }
