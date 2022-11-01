c21b8dd80fa108aedaf838e8cd9e137e471375df
bro_code <kelimbth@gmail.com>
2022-11-01 12:07:33 +0300

    added a linear gradient

--- lib/screens/login_screen/login.dart
+++ lib/screens/login_screen/login.dart
@@ -9,7 +9,21 @@ class LoginScreen extends StatefulWidget {
 
 class _LoginScreenState extends State<LoginScreen> {
   Widget topWidget(double screenWidth) {
-    return Container();
+    return Container(
+      width: 1.2 * screenWidth,
+      height: 1.2 * screenWidth,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(120),
+        gradient: LinearGradient(
+          begin: Alignment(-0.2, -0.8),
+          end: Alignment.bottomCenter,
+          colors: [
+            Color(0x007CBFCF),
+            Color(0xB316BFC4),
+          ],
+        ),
+      ),
+    );
   }
   @override
   Widget build(BuildContext context) {
