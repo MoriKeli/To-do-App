5e74476cde5c92c3c0c976c5c5f0bd892cbba9c4
bro_code <kelimbth@gmail.com>
2022-11-01 11:42:23 +0300

    created a new UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,27 @@
 import 'package:flutter/material.dart';
-import 'package:new_app/pages/home.dart';
+import 'package:new_app/utils/constants.dart';
 
 void main() => runApp(MaterialApp(
-  home: Home(),
-  theme: ThemeData(primarySwatch: Colors.blue),
+  home: LoginScreen(),
+  debugShowCheckedModeBanner: false,
+  
 ));
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    theme: ThemeData(
+      scaffoldBackgroundColor: kBackgroundColor,
+      textTheme: Theme.of(context).textTheme.apply(bodyColor: kPrimaryColor),
+    );
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login Screen'),
+        centerTitle: true,
+
+      ),
+    );
+  }
+}
