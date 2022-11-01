d551f7eb1b62b9afdc1694fb9f0b4d5720fde9e0
bro_code <kelimbth@gmail.com>
2022-11-01 12:34:32 +0300

    configured and styled CenterWidget()

--- lib/screens/center_widget/center_widget.dart
+++ lib/screens/center_widget/center_widget.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
 
 class CenterWidget extends StatelessWidget {
-  const CenterWidget({super.key});
+  final Size size;
+  const CenterWidget({super.key, required this.size});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Stack(
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment(1, -0.6),
+              end: Alignment(-1, 0.8),
+              colors: [
+                Color.fromRGBO(61, 232, 150, 0.502),
+                Color.fromRGBO(118, 227, 174, 0.302),
+              ]
+            ),
+          ),
+        )
+      ],
+    );
   }
 }
